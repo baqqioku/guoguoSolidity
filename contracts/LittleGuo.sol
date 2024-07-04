e3f19e4dbf5adfbfb482d6c740d188a9074051cd
guoguo <576034485@qq.com>
2024-07-05 01:22:21 +0800

    four commit

---
+++ contracts/LittleGuo.sol
@@ -0,0 +1,10 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract LittleGuo {
+    struct Guo {
+        string name;
+        int8 age;
+    }
+}
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.24;
 
 contract SimpleStorage {
+
+    
     uint256 public num;
     function set(uint256 _num) public{
         num = _num;
